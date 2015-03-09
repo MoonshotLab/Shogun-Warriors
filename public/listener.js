a343b613b3e0ad54760a26b444b981f69da91c23
Joe Longstreet <joelongstreet@gmail.com>
2015-03-09 18:44:01 -0500

    make the words animate in

--- public/listener.js
+++ public/listener.js
@@ -1,10 +1,30 @@
 $(function(){
 
   socket.on('new-tweet', function(tweet){
-    $('.poster').css({
-      'background' : backgrounds.getOne()
-    });
-    $('.tweet').html(tweet.parsed);
-  });
+    $('.tweet').addClass('animate');
+    setTimeout(function(){
+      $('.tweet').addClass('hide');
+    }, 1);
 
+    setTimeout(function(){
+      $('.tweet').removeClass('hide');
+      $('.poster').css({ 'background' : backgrounds.getOne() });
+
+      $('.tweet').html(tweet.parsed);
+      $('.tweet').find('span').each(function(i){
+        scheduleReveal($(this), i);
+      });
+    }, 1500);
+
+  });
 });
+
+
+var scheduleReveal = function($el, i){
+  $el.addClass('animated');
+  $el.addClass('hide');
+  setTimeout(function(){
+    $el.removeClass('hide');
+    $el.addClass('flipInX');
+  }, i*100);
+};
