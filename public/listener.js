98a52d435698ed6e35b87e372ceb60e4e0324bd7
Joe Longstreet <joelongstreet@gmail.com>
2015-03-09 21:29:44 -0500

    order of word transitions is now randomized

--- public/listener.js
+++ public/listener.js
@@ -1,49 +1,64 @@
+var $tweet;
+var animationClass = 'flipInX';
+// var animationClass = 'flipInY';
+// var animationClass = 'bounceIn';
+// var animationClass = 'bounceInUp';
+// var animationClass = 'fadeInUpBig';
+// var animationClass = 'zoomInUp';
+
+
 $(function(){
   toggleBackground();
-  var $tweet = $('.tweet')
+  $tweet = $('.tweet');
 
   socket.on('new-tweet', function(tweet){
     $tweet.addClass('animate');
-    setTimeout(function(){
-      $tweet.addClass('hide');
-    }, 1);
 
-    setTimeout(toggleBackground, 700);
-
-    setTimeout(function(){
-      var text   = '';
-      var splits = tweet.parsed.split(' ');
-
-      splits.forEach(function(split){
-        var sub = '';
-        if(split.indexOf('span') == -1){
-          sub += '<span class="other">';
-          sub += split;
-          sub += '</span> ';
-        } else sub = (split + ' ');
-
-        text += sub;
-      });
-
-      $tweet.removeClass('hide');
-      $tweet.html(text);
-      $tweet.find('span').each(function(i){
-        scheduleReveal($(this), i);
-      });
-    }, 1500);
-
+    setTimeout(function(){ $tweet.addClass('hide');}, 1);
+    setTimeout(toggleBackground,                      700);
+    setTimeout(function(){ updateText(tweet); },      1500);
   });
 });
 
 
 
+var updateText = function(tweet){
+
+  // ensure every word is surrounded in a span
+  var text   = '';
+  var splits = tweet.parsed.split(' ');
+  splits.forEach(function(split){
+    var subStr = '';
+    if(split.indexOf('span') == -1){
+      subStr += '<span class="other">';
+      subStr += split;
+      subStr += '</span> ';
+    } else subStr = (split + ' ');
+
+    text += subStr;
+  });
+
+  // unfade the text and use the new tweet
+  $tweet.removeClass('hide');
+  $tweet.html(text);
+
+  // loop over each element
+  var spans = shuffleArray($tweet.find('span'));
+  spans.each(function(i){
+    scheduleReveal($(this), i);
+  });
+};
+
+
+
+// just wait a little bit before showing the word
 var scheduleReveal = function($el, i){
   $el.addClass('animated');
   $el.addClass('hide');
   setTimeout(function(){
     $el.removeClass('hide');
-    $el.addClass('flipInX');
-  }, (i+1)*100);
+    $el.addClass(animationClass);
+  }, (i+1)*50);
 };
 
 
@@ -60,4 +75,13 @@ var toggleBackground = function(){
     $a.addClass('hide');
     $b.removeClass('hide');
   }
+};
+
+
+
+var shuffleArray = function(a) {
+  var i = a.length, t, j;
+  a = a.slice();
+  while (--i) t = a[i], a[i] = a[j = ~~(Math.random() * (i+1))], a[j] = t;
+  return a;
 };
