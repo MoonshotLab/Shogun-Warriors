8af57876bb993eaacc084cb25aa7a147aae6604a
Joe Longstreet <joelongstreet@gmail.com>
2015-04-02 16:01:11 -0500

    make retweet and screenshot creation optional

--- index.js
+++ index.js
@@ -61,18 +61,25 @@ setInterval(function(){
 // send tweets to the client and remove them once used
 twitter.stream.on('tweet', function(tweet){
   console.log('new tweet', tweet.text);
+
+  // this is a hack to prevent the auto tweet replies from being picked up
+  // and treated like a new tweet. It would create and endless loop
+  // of tweets and creenshot creations
   if(tweet.text.indexOf('Thanks for visiting us') == -1){
     utils.preparseTweet(tweet, function(parsedTweet){
       parsedTweet.hash_tag = config.HASH_TAG;
       tweetQueue.push(parsedTweet);
 
-      db.saveTweet(parsedTweet)
-        .then(utils.createScreenshot)
-        .then(s3.rememberScreenshot)
-        .then(twitter.respondToUser)
-        .fail(function(err){
-          console.log(err);
-        });
+      if(config.REPLY_TO_TWEETS){
+        db.saveTweet(parsedTweet)
+          .then(utils.createScreenshot)
+          .then(s3.rememberScreenshot)
+          .then(twitter.respondToUser)
+          .fail(function(err){ console.log(err); });
+      } else {
+        db.saveTweet(parsedTweet)
+          .fail(function(err){ console.log(err); });
+      }
     });
   }
 });
